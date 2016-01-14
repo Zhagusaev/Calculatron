9ff224acc49ad2bbafd3a934350ba2b13ae53a5a
erd-s <chris.erdos@gmail.com>
2016-01-14 11:26:17 -0600

    we hooked up some buttons to an IBAction

--- Calculatron2/ViewController.m
+++ Calculatron2/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *operandOne;
+@property (weak, nonatomic) IBOutlet UITextField *operandTwo;
+@property (weak, nonatomic) IBOutlet UILabel *operation;
 
 @end
 
@@ -16,12 +19,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)onOperationTapped:(UIButton *)sender {
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @end
