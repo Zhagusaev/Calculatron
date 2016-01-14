fc0144d6e4302566e0c75ee1a8b81112ec696779
Kanybek Zhagusaev <zhagusaev@gmail.com>
2016-01-14 10:22:07 -0600

    Initial Commit

---
+++ Calculatron2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Calculatron2
+//
+//  Created by Kanybek Zhagusaev on 1/14/16.
+//  Copyright © 2016 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
